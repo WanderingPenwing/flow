5b57596ce0bca75ee52f294e29f4b1e66daf8f18
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-07-23 16:45:08 +0200

    second try to switch to gnu target

--- shell.nix
+++ shell.nix
@@ -9,6 +9,7 @@ mkShell {
     libXi
     pkg-config
   ] ++ [
+    rustup
     cargo
     rustc
     atk
@@ -21,14 +22,21 @@ mkShell {
     gtk3-x11
     gnome.zenity
   ];
+
   buildInputs = [
     latest.rustChannels.stable.rust
     xorg.libX11
     wayland
     libxkbcommon
+    zlib
+    glibc
   ];
 
   shellHook = ''
-      export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${lib.makeLibraryPath ([libGL libGLU libxkbcommon])}
+    export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${lib.makeLibraryPath ([libGL libGLU libxkbcommon glibc])}:/nix/store/dbcw19dshdwnxdv5q2g6wldj6syyvq7l-glibc-2.39-52/lib
+    rustup toolchain install stable
+    rustup default stable
+    rustup target add x86_64-unknown-linux-gnu
+    rustup target add x86_64-unknown-linux-musl
   '';
 }
