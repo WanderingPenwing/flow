d54cba4e4b4b4acd07da41a958740c593f1a043d
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-08-05 18:51:17 +0200

    cleaned up

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ use winit::event_loop::EventLoop;
 
 fn main() {
     match EventLoop::new() {
-        Ok(event_loop) => {
-            // Event loop was created successfully, you can use `event_loop` here
+        Ok(_event_loop) => {
             println!("Event loop created successfully");
         }
         Err(e) => {
-            // Handle the error
             eprintln!("Failed to create event loop: {:?}", e);
         }
     }
